b5425874da25242a34a91f1c9e9ef58c130d8e3b
Nedesh <andrey.deshankov@gmail.com>
2023-04-24 15:59:08 +0300

    Initial commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,16 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+        System.out.println(bean == bean2);
+
+        Cat catBean = (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean.getMessage());
+
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean.getMessage());
+        System.out.println(catBean == catBean2);
     }
 }
